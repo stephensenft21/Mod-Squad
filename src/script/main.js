0f8b8f733a772f11ea0900162ac581d4caea3d78
Stephen Senft <stephensenft21@gmail.com>
2019-03-11 10:27:04 -0500

    added comments to file

--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,21 @@
+   
    const showInfo = document.querySelector(".showInfo")
-    const ModSquad = {
+   
+   const ModSquad = {
 
         members: ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
-
+//added a comma at the end of the array. 
         series: {
             "start": "1968",
             "end": "1973"
         }
     }
-
+//changed my "const" to a "let" variable
    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
 
     ModSquad.members.forEach(member => {
         HTMLRepresentation += `<div>${member}</div>`
     })
 
-
+//added an article tag to the DOM to hold the "showInfo" class
 showInfo.innerHTML = HTMLRepresentation
